66e54bfd266897251d8785d333cca65d398c3043
RISHIKESH KUMAR <the00cracked00coder@gmail.com>
2021-04-23 07:58:26 +0530

    Updated settings

--- .bash_profile
+++ .bash_profile
@@ -1,2 +1,4 @@
 export MAIL_USER="the00cracked00coder@gmail.com"
 export MAIL_PASS="atvalkusuyadmolj"
+export SECRET_KEY="72177c89b4971f8bbf596c1a89218d2582a17a04eadc112f"
+export DEBUG_VALUE="True"
--- bloger/settings.py
+++ bloger/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0_x9wltlycnh6nr4&x!tb2u1_a1yir-8_q7-sldxysz-!@o2k5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meradjapp.herokuapp.com']
 
 
 # Application definition
